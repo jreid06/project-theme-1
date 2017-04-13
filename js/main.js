3bead8bad442dbd9ee270aa648ecb56c01f98026
Jason Reid <jasonbrandon9406@gmail.com>
2017-04-13 23:30:51 +0100

    project-theme-1 files-13th April

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-  
+
+  function loaderIcon() {
+      var imgb1 = $('.img-box1');
+      var imgb2 = $('.img-box2');
+
+      setTimeout(function(){
+        $(imgb1).css({"animation-name": "rotateRound360", "animation-iteration-count": "infinite"});
+        $(imgb2).css({"animation-name": "rotateRound360", "animation-iteration-count": "infinite"});
+      }, 14100);
+  }
+
+  // loaderIcon();
 
 });
