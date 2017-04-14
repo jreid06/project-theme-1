2eddcd13005d94219e1ec4f223210b8941c2d519
Jason Reid <jasonbrandon9406@gmail.com>
2017-04-14 12:47:17 +0100

    refactored countdown functions

--- js/main.js
+++ js/main.js
@@ -24,77 +24,161 @@ $(document).ready(function() {
     //SET UP OTHER PAGES AND FIX DESIGN QUIRKS
     //REFACTOR CODE WHERE POSSIBLE
 
-    function countdown(time) {
-      switch (time) {
-        case 1:
-            setTimeout(function(){
-              time = 0;
-              $('#time01').text(time);
-              return timedsd(time);
 
-            }, 1000);
-          break;
-          case 2:
-            setTimeout(function(){
-              time = 1;
-              $('#time01').text(time);
-              return timedsd(time);
 
-            }, 1000);
-          break;
-        case 3:
-            setTimeout(function(){
-              time = 2;
-              $('#time01').text(time);
-              return timedsd(time);
+    //REFACTORED CODE
 
-            }, 1000);
-          break;
-        case 4:
-            setTimeout(function(){
-                time = 3;
-                $('#time01').text(time);
-              return timedsd(time);
+    var startpos = 5;
+    var timeptag = $('#time01');
 
-            }, 1000);
-          break;
-        case 5:
-            setTimeout(function(){
-              time = 4;
-              $('#time01').text(time);
-              return timedsd(time);
-            }, 1000);
-          break;
+    function countD() {
+      console.log("countdown initialize");
+      $(timeptag).text(startpos);
+
+      if (startpos > 0) {
+        console.log("function started");
+        timeingFunc();
       }
     }
 
-    function timedsd(cdval) {
-      if (cdval === 5) {
-        countdown(cdval);
-        console.log("first if ran");
-      }
-      else if (cdval === 4) {
-        countdown(cdval);
-        console.log("second if ran");
-      }
-      else if (cdval === 3) {
-        countdown(cdval);
-        console.log("third if ran");
-      }
-      else if (cdval === 2) {
-        countdown(cdval);
-        console.log("fourth if ran");
-      }
-      else if (cdval === 1) {
-        countdown(cdval);
-        console.log("fourth if ran");
-      }
-
-      else if (cdval === 0) {
-        x = 7;
-        console.log("COUNTDOWN PROCESS STOPPED");
-      }
+    function clearIval(intf) {
+      clearInterval(intf);
     }
+
+    function timeingFunc() {
+        var x = setInterval(function(){
+
+            switch (startpos) {
+              case 5:
+                startpos -= 1;
+                $(timeptag).text(startpos);
+                return startpos;
+                break;
+
+              case 4:
+                startpos -= 1;
+                $(timeptag).text(startpos);
+                return startpos;
+                break;
+
+              case 3:
+                startpos -= 1;
+                $(timeptag).text(startpos);
+                return startpos;
+                break;
+
+              case 2:
+                startpos -= 1;
+                $(timeptag).text(startpos);
+                return startpos;
+                break;
+
+              case 1:
+                startpos -= 1;
+                $(timeptag).text(startpos);
+                return startpos;
+                break;
+
+              case 0:
+                $(timeptag).text("ENTER");
+                console.log("program finished");
+                clearIval(x);
+                break;
+              default:
+
+            }
+          // console.log("Im about to run");
+          // startpos -= 1;
+          // console.log("number: " + startpos);
+          // if (startpos === 0) {
+          //   console.log("time to stop");
+          //   countD();
+          //   return startpos;
+          //
+          // }
+        }, 1000);
+    }
+
+    countD();
+
+
+
+
+
+
+    //OLD CODE
+
+    // function countdown(time) {
+    //   switch (time) {
+    //     case 1:
+    //         setTimeout(function(){
+    //           time = 0;
+    //           $('#time01').text(time);
+    //           return timedsd(time);
+    //
+    //         }, 1000);
+    //       break;
+    //       case 2:
+    //         setTimeout(function(){
+    //           time = 1;
+    //           $('#time01').text(time);
+    //           return timedsd(time);
+    //
+    //         }, 1000);
+    //       break;
+    //     case 3:
+    //         setTimeout(function(){
+    //           time = 2;
+    //           $('#time01').text(time);
+    //           return timedsd(time);
+    //
+    //         }, 1000);
+    //       break;
+    //     case 4:
+    //         setTimeout(function(){
+    //             time = 3;
+    //             $('#time01').text(time);
+    //           return timedsd(time);
+    //
+    //         }, 1000);
+    //       break;
+    //     case 5:
+    //         setTimeout(function(){
+    //           time = 4;
+    //           $('#time01').text(time);
+    //           return timedsd(time);
+    //         }, 1000);
+    //       break;
+    //   }
+    // }
+    //
+    // function timedsd(cdval) {
+    //   if (cdval === 5) {
+    //     countdown(cdval);
+    //     console.log("first if ran");
+    //   }
+    //   else if (cdval === 4) {
+    //     countdown(cdval);
+    //     console.log("second if ran");
+    //   }
+    //   else if (cdval === 3) {
+    //     countdown(cdval);
+    //     console.log("third if ran");
+    //   }
+    //   else if (cdval === 2) {
+    //     countdown(cdval);
+    //     console.log("fourth if ran");
+    //   }
+    //   else if (cdval === 1) {
+    //     countdown(cdval);
+    //     console.log("fourth if ran");
+    //   }
+    //
+    //   else if (cdval === 0) {
+    //     x = 7;
+    //     console.log("COUNTDOWN PROCESS STOPPED");
+    //   }
+    // }
 
     // var x = 5;
 
@@ -110,11 +194,11 @@ $(document).ready(function() {
   //     },1000);
   //   }
   //
-  // if (x < 6) {
+  //   if (x < 6) {
   //     counter();
   // }
 
-  alert("new branch COUNTDOWN 01");
+    // alert("new branch COUNTDOWN 01");
 
 
     // var count = 0;
@@ -127,6 +211,7 @@ $(document).ready(function() {
     //     }, 1000);
     //   }
     // }
+
 
 
 
